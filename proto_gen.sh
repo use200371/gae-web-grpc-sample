38aaebfdbea488ad6d41bfde4bf1ee32681a3f98
sn071 <k-ozeki@use-eng.co.jp>
2020-10-17 17:14:04 +0900

    script for mac update

--- proto_gen.sh
+++ proto_gen.sh
@@ -1,14 +1,27 @@
-  
 #/bin/sh
 
 set -eu
-
-#export PATH="$PATH"
 
 PROTO_SRC=./proto
 PROTO_DEST=./src/proto
 
 mkdir -p ${PROTO_DEST}
+
+PROTOC=$(npm bin)/../grpc-tools/bin
+PROTOC_GEN_GRPC_WEB=
+
+if [ "$(uname)" == 'Darwin' ]; then
+  PROTOC=$PROTOC/protoc
+  PROTOC_GEN_GRPC_WEB=$(pwd)/protoc-gen-grpc-web
+else
+  PROTOC=$PROTOC/protoc.exe
+  PROTOC_GEN_GRPC_WEB=./protoc-gen-grpc-web.exe
+fi
+
+if [ ! -f $PROTOC_GEN_GRPC_WEB ]; then
+  echo -e "Please program Download!\nhttps://github.com/grpc/grpc-web/releases"
+  exit 1
+fi
 
 npx grpc_tools_node_protoc \
   --js_out=import_style=commonjs,binary:${PROTO_DEST} \
@@ -21,8 +34,8 @@ npx grpc_tools_node_protoc \
   -I ${PROTO_SRC} \
   ${PROTO_SRC}/*
 
-node_modules/grpc-tools/bin/protoc.exe \
-  --plugin=./protoc-gen-grpc-web.exe \
+$PROTOC \
+  --plugin=$PROTOC_GEN_GRPC_WEB \
   --grpc-web_out=import_style=commonjs,mode=grpcwebtext:${PROTO_DEST} \
   -I ${PROTO_SRC} \
   ${PROTO_SRC}/*
