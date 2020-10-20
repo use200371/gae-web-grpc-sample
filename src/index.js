22c9c560920c4f227b58da190bb9148a46cfe9ae
use200371 <k-ozeki@use-eng.co.jp>
2020-10-20 19:29:26 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {HelloRequest, HelloReply} from './proto/helloworld_pb.js';
 import {GreeterClient} from './proto/helloworld_grpc_web_pb.js';
 
-var client = new GreeterClient('http://' + window.location.hostname + ':8000', null, null);
+var client = new GreeterClient('http://' + window.location.hostname + ':' + window.location.port, null, null);
 
 // simple unary call
 var request = new HelloRequest();
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import morgan from 'morgan';
 
 const grpcExpress = require('grpc-express');
 
+const HTTP_PORT:Number = 9000;
+
 class HelloService implements hello_grpc_pb.IGreeterServer 
 {
     sayHello(
@@ -27,31 +29,82 @@ const client = new hello_grpc_pb.GreeterClient(
 
 class InProxy
 {
+    private _unaryCalls:Map<String, String> = new Map();
+
+    constructor(client:any)
+    {
+        [
+            ...new Set(Object.keys(client.__proto__).filter(
+                route => !client[route].requestStream && !client[route].responseStream
+            ).map(route => client[route].path))
+        ].filter(r => r).forEach(route => {
+            const keys = this.routeToPathKeys(route);
+            this._unaryCalls.set(keys.lower, keys.name);
+            this._unaryCalls.set(keys.upper, keys.name);
+        });
+
+    }
+
+    routeToPathKeys(route:any):any {
+        const functionName = route.split('/').slice(-1)[0];
+        return {
+            upper: route.replace(functionName, this.toUpperCaseFirstLetter(functionName)),
+            lower: route.replace(functionName, this.toLowerCaseFirstLetter(functionName)),
+            name: this.toLowerCaseFirstLetter(functionName)
+        };
+    }
+    
+    toUpperCaseFirstLetter(string:String):String 
+    {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    toLowerCaseFirstLetter(string:String):String
+    {
+        return string.charAt(0).toLowerCase() + string.slice(1);
+    }    
+
     proxy(req:express.Request, res:express.Response, next:express.NextFunction)
     {
+        console.log(req.path);
+
+        if (!this._unaryCalls.has(req.path)) {
+            next();
+            return;
+        }
+
+    //if (this._unaryCalls.has(req.path)) this.proxyUnaryCall(req, res);
+    // else if (this._serverStreamCalls.has(req.path))
+    //   this.proxyServerStreamCall(req, res);
+    // else next();
+
         (async ()=> {
 
-            res.setHeader('Content-Type', 'application/json');
+            res.setHeader('Content-Type', 'application/grpc-web-text');
 
-            const req2 = new hello_pb.HelloRequest();
-            req2.setName('Hello World');
+            const grpcreq = new hello_pb.HelloRequest();
+            grpcreq.setName('Hello World');
             
-            client.sayHello(req2, function(error, result) {
+            const test = await client.sayHello(grpcreq,  (error, result) => {
                 if (error) 
                 {
                     console.log('Error: ', error);
                 }
                 else 
                 {
-                    const ret = result.toObject();
-                    res.json(ret);
+                    console.log(result.toObject());
+                    //const ret =  result.toObject();
+                    //const ret = Buffer.from(result.toObject()).toString('base64')
+                    //res.json(ret);
+                    res.status(200).send(result);
                     return;
                 }
             });
 
+            //res.status(200).send('test');
+
         })();
         
-        next();
+        //next();
     }
 }
 
@@ -64,10 +117,10 @@ class InProxy
         grpc.credentials.createInsecure(),
     );
 
-    app.listen(8000, async ()=> {
+    app.listen(HTTP_PORT, async ()=> {
         console.log(__dirname);
-        const test = new InProxy();
-        app.use(test.proxy);
+        const test = new InProxy(client);
+        app.use(test.proxy.bind(test));
         app.use(express.static(path.join(__dirname, '../dist')));
         app.use(morgan('combined'));
 
